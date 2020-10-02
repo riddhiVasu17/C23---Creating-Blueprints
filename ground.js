ccfe7beab504e9126a629c8f0afb83449a8ef5f3
Riddhi Vasu <69344418+riddhiVasu17@users.noreply.github.com>
2020-10-02 13:35:45 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,24 @@
+class ground {
+
+    constructor(x, y, width, height) {
+    
+    var option = {
+        isStatic : true
+    }
+    
+    this.body = Bodies.rectangle(x, y, width, height, option);
+    World.add(world, this.body);
+    
+    this.width = width;
+    this.height = height;
+
+    }
+
+    display() {
+    var pos = this.body.position ;
+
+    rectMode(CENTER);
+    rect(pos.x, pos.y, this.width, this.height);
+
+    }
+}
